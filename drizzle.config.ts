2fc2b7625ef4abdc92e11f01e738c46472770cd7
Matey <mateyilkiv@gmail.com>
2024-04-20 13:56:09 +0200

    db setup

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,4 +1,4 @@
-import { type Config } from "drizzle-kit";
+import type { Config } from "drizzle-kit";
 
 import { env } from "~/env";
 
@@ -6,7 +6,7 @@ export default {
   schema: "./src/server/db/schema.ts",
   driver: "pg",
   dbCredentials: {
-    connectionString: env.DATABASE_URL,
+    connectionString: env.POSTGRES_URL,
   },
   tablesFilter: ["t3gallery_*"],
 } satisfies Config;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import { db } from "~/server/db";
 
 const mockUrls = [
   "https://utfs.io/f/c24903cc-26bb-4daf-af75-2a0efe571b88-1x1slq.jpeg",
@@ -11,12 +12,18 @@ const mockImages = mockUrls.map((url) => ({
   url,
 }));
 
-export default function HomePage() {
+export default async function HomePage() {
+  const posts = await db.query.posts.findMany();
+
   return (
     <main>
       <div className="flex flex-wrap gap-3">
+        {posts.map((post) => (
+          <div key={post.id}>{post.name}</div>
+        ))}
+
         {[...mockImages, ...mockImages, ...mockImages].map((image) => (
-          <div key={image.id} className="w-48">
+          <div key={image.id + crypto.randomUUID()} className="w-48">
             <Image src={image.url} alt="image" width={1000} height={200} />
           </div>
         ))}
